42c9342f0508a2faccd0ac292c2c6d9b1b52121e
akilan selvacoumar <akilanselva@hotmail.com>
2019-07-27 19:07:48 +0400

    Generating website module

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -15,6 +15,8 @@ exports.generate = function(req, res) {
     //we can now manipulate the data
     let result = req.body.companyName;
 
+    //console.log(req.body);
+
     let dir = './public/pages/' + req.body.companyName;
 
     if (!fs.existsSync(dir)) {
@@ -36,8 +38,27 @@ exports.generate = function(req, res) {
     console.log('great, here is the request body ->', req.body);
   
     //data.push(result);
+
+    let template = fs.readFileSync("./public/template_details.json");
+    let templateContent = JSON.parse(template);
+
+    for(var i = 0;i < templateContent.templates.length; i++){
+      if(templateContent.templates[i].template_name == "Colo_Shop"){
+
+        for(var j = 0; j < templateContent.templates[i].pages.length; j++){
+          //Copying html files can be removed in the future stages
+          fs.createReadStream('./public/templates/'+req.body.template+'/'+templateContent.templates[i].pages[j]+'.html').pipe(fs.createWriteStream(dir+'/'+templateContent.templates[i].pages[j]+'.html'));
+          ejs2html('./public/templates/'+req.body.template+'/'+templateContent.templates[i].pages[j]+'.ejs', req.body , dir, "index");
+        }
+
+        for(var j = 0; j < templateContent.templates[i].folder.length; j++){
+          copy_folder('./public/templates/'+req.body.template+'/'+templateContent.templates[i].folder[j], dir + '/'+templateContent.templates[i].folder[j]);
+        }
+
+      }
+    }
   
-    fs.createReadStream('./public/Colo_Shop/index.html').pipe(fs.createWriteStream(dir + '/index.html'));
+    /*fs.createReadStream('./public/Colo_Shop/index.html').pipe(fs.createWriteStream(dir + '/index.html'));
     fs.createReadStream('./public/Colo_Shop/cart.html').pipe(fs.createWriteStream(dir + '/cart.html'));
     fs.createReadStream('./public/Colo_Shop/contact.html').pipe(fs.createWriteStream(dir + '/contact.html'));
   
@@ -48,7 +69,7 @@ exports.generate = function(req, res) {
   
     ejs2html('./public/Colo_Shop/index.ejs', req.body , dir, "index");
     ejs2html('./public/Colo_Shop/cart.ejs', req.body , dir, "cart");
-    ejs2html('./public/Colo_Shop/contact.ejs', req.body , dir, "contact");
+    ejs2html('./public/Colo_Shop/contact.ejs', req.body , dir, "contact");*/
     
     const resp = {
       'directory': result
@@ -58,7 +79,7 @@ exports.generate = function(req, res) {
 }
 
 exports.convertCsv = function(req,res,next){
-
+    console.log(req.body);
     const csvStr =  req.body.csv;
     csv()
     .fromString(csvStr)
@@ -86,6 +107,7 @@ exports.validateData = function(req, res){
     logoUrl: Joi.string().uri().required(),
     bannerUrl: Joi.string().uri().required(),
     email: Joi.string().email().required(),
+    template: Joi.string().required(),
     description: Joi.string().required(),
     csv: Joi.string().required(),
 })
@@ -125,7 +147,7 @@ function ejs2html(path, information, dir, name) {
       html = template(information);
     fs.writeFile(path + '.html', html, function (err) {
       if (err) { console.log(err); return false }
-      fs.createReadStream('./public/Colo_Shop/'+name+'.ejs.html').pipe(fs.createWriteStream(dir + '/'+name+'.html'));
+      fs.createReadStream('./public/templates/'+information.template+'/'+name+'.ejs.html').pipe(fs.createWriteStream(dir + '/'+name+'.html'));
       return true;
     });
   });
